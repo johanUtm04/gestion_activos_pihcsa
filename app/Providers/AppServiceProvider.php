89cb70ea4d0abcbdfb693abfafdd69170a356108
johanUtm04 <johanlopezrey1@gmail.com>
2026-03-02 17:34:07 -0600

    Fix: Desactivar forceScheme HTTPS para entorno local/producción sin SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Paginator::useBootstrap();
         if (app()->environment('production')) {
-        URL::forceScheme('https');
+        URL::forceScheme('http');
     }
     }
 }
