cf4113f1e696f54f4b9ca461b188402e51705734
johanUtm04 <johanlopezrey1@gmail.com>
2026-03-04 09:12:58 -0600

    feat: depreciacionController identado

--- app/Http/Controllers/DepreciacionController.php
+++ app/Http/Controllers/DepreciacionController.php
@@ -1,23 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Equipo;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 /*
 |--------------------------------------------------------------------------
-| Controlador destinado para acciones depreciacion
+| DepreciacionController
 |--------------------------------------------------------------------------
+| Gestiona el cálculo y la visualización del valor contable de los activos
+| a lo largo del tiempo.
+|
 */
 
 class DepreciacionController extends Controller
 {
+    /**
+     * Muestra el listado de equipos con su información de depreciación.
+     * * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request): View
+    {
+        // Se recuperan los equipos con paginación para optimizar la carga de memoria
+        $equipos = Equipo::paginate(10);
 
-//Metodo para mostrar vista
-public function index(Request $request)
-{
-    $equipos = Equipo::paginate(10);
         return view('depreciacion.index', compact('equipos'));
     }
-    
 }
